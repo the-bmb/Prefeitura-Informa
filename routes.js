ea6427249ecf13fb5c2920debeba119eedbe46e9
bruna.braga <bruna.braga@primeinterway.com.br>
2020-07-07 16:12:08 -0300

    add: comentários nas rotas

--- routes.js
+++ routes.js
@@ -3,11 +3,17 @@ const routes = express.Router();
 
 const NotificationController = require("./controllers/NotificationController");
 const UserController = require("./controllers/UserController");
+//trás todas as notificações
 routes.get("/notification", NotificationController.index);
+//cria uma nova notificação
 routes.post("/notification", NotificationController.store);
+//login de usuário da prefeitura
 routes.post("/login", UserController.index);
+//trás os usuários que ainda não foram aprovados
 routes.get("/toAprove", UserController.toAprove);
+//cria um novo usuário
 routes.post("/user", UserController.store);
+//edita um usuário
 routes.put("/user/:id", UserController.update);
 
 module.exports = routes;
